c4bed444b3b85642455ac984401c62fde8a7820d
IlanaShukhman <57867566+IlanaShukhman@users.noreply.github.com>
2022-06-13 00:40:57 +0300

    Delete flask-example directory

--- flask-example/input.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import (StringField, TextAreaField, IntegerField, BooleanField,
-                     RadioField)
-from wtforms.validators import InputRequired, Length
-
-class CourseForm(FlaskForm):
-    size = IntegerField('Size', validators=[InputRequired()])
-    k = IntegerField('k', validators=[InputRequired()])
